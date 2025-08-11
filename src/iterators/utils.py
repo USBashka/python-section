942978e2dce79f6111b330ed76abbcf12cc9b089
USBashka <usbashka@gmail.com>
2025-08-11 16:45:44 +0400

    Task 3

--- src/iterators/utils.py
+++ src/iterators/utils.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 from itertools import batched
-from typing import Iterable, TypeAlias
+from typing import Iterable, TypeAlias, Iterator
 
 SomeRemoteData: TypeAlias = int
 
@@ -29,8 +29,50 @@ def request(query: Query) -> Page:
 
 
 class RetrieveRemoteData:
-    pass
+    """Класс-генератор постраничной выборки из API"""
+    def __init__(self, per_page: int = 3):
+        self.per_page = per_page
+
+    def __iter__(self) -> Iterator[SomeRemoteData]:
+        def gen() -> Iterator[SomeRemoteData]:
+            page_num = 1
+            while True:
+                page = request(Query(per_page=self.per_page, page=page_num))
+                for item in page.results:
+                    yield item
+                if page.next is None:
+                    break
+                page_num = page.next
+        return gen()
 
 
 class Fibo:
-    pass
+    """Итератор последовательности Фибоначчи от 0 длиной n"""
+    def __init__(self, n: int):
+        if not isinstance(n, int) or n < 0:
+            raise ValueError("N must be a non-negative integer")
+        self.n = n
+        self._i = 0
+        self._a = 0
+        self._b = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self) -> int:
+        if self._i >= self.n:
+            raise StopIteration
+        value = self._a
+        self._a, self._b = self._b, self._a + self._b
+        self._i += 1
+        return value
+
+
+
+def main():
+    print(list(Fibo(10)))
+    print(list(RetrieveRemoteData(per_page=3)))
+
+
+if __name__ == "__main__":
+    main()
